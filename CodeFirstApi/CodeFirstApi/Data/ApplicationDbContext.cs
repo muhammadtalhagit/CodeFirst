f9c156d23592435825b51f73a7fa305f27a87e50
asp <asp@aptech.com>
2024-09-27 13:02:13 +0500

    Code first

---
+++ CodeFirstApi/CodeFirstApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CodeFirstApi.Controllers.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Brand> Brands { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ CodeFirstApi/CodeFirstApi/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeFirstApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int BrandId { get; set; }
+
+        public virtual Brand Brand { get; set; }
+
+
+    }
+}
